909a778364bfee6435ce1d01fdbd280cd92d3c59
Ricardo Diaz <kerackdarcku@gmail.com>
2024-03-25 06:38:28 -0500

    Init Back

---
+++ Back/src/main/java/com/mindhub/ticketmind/TicketmindApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.ticketmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmindApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/com/mindhub/ticketmind/TicketmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.ticketmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
