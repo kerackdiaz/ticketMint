2f12e619d76d057089e9724a7c80e2110fcee62e
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-03-28 21:30:37 -0300

    implementacion de datos de usuario

--- TicketMint/src/pages/user/DetailsEvent.jsx
+++ TicketMint/src/pages/user/DetailsEvent.jsx
@@ -7,20 +7,14 @@ import { FaRegClock } from "react-icons/fa6";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { BsTicketPerforated } from "react-icons/bs";
 import { Link, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
 function DetailsEvent() {
   const [onFav, setOnFav] = useState(false)
   const [quantity, setQuantity] = useState(1)
+  const event = useSelector((state) => state.authReducer.events)
   const {id} = useParams()
-
-  /* useEffect(() => {
-    axios.get(`https://ticket-mint-api.herokuapp.com/events/${id}`,{
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-  },[]) */
 
     const handleFav = () => {
         setOnFav(!onFav)
@@ -55,12 +49,14 @@ function DetailsEvent() {
       }
     }
 
+    const even = Object.values(event).find((event) => event.id === id)
+
   return (
-    <div className='bg-[url(/prueba.png)] bg-contain md:bg-cover md:bg-no-repeat flex flex-col items-center justify-end w-screen h-screen bg-top '>
+    <div className={'bg-[url("'+ even.imageURL +'")] bg-contain md:bg-cover md:bg-no-repeat flex flex-col items-center justify-end w-screen h-screen bg-top '}>
       <div className='bg-[#55347b] p-3 w-full rounded-t-2xl md:w-1/2'>
         <div className=''>
           <div className='flex justify-between items-center '>
-            <h2 className='text-white text-2xl font-bold'>Arch Enemy</h2>
+            <h2 className='text-white text-2xl font-bold'>{even.name}</h2>
             {
                 !onFav ? <CiStar className='text-4xl text-white cursor-pointer' onClick={handleFav}/> : <TiStarFullOutline className=' cursor-pointer text-4xl text-white' onClick={handleFav}/>
             }
@@ -68,11 +64,12 @@ function DetailsEvent() {
 
           <div className='flex justify-between border-b-2 border-[#8468fb] p-3'>
             <div className='flex flex-col items-start opacity-90'>
-              <p className='text-white pb-3 text-sm items-center flex gap-1'><FaRegCalendarAlt/>Sunday, November 27, 2024</p>
-              <p className='text-white pb-3 text-sm items-center flex gap-1'><BsTicketPerforated/>Only 10 tickets left</p>
-              <p className='text-white text-sm items-center flex gap-1'><FaRegClock/>9:00 PM - 10:00 PM</p>
+              <p className='text-white pb-3 text-sm items-center flex gap-1'><FaRegCalendarAlt/>{even.date}</p>
+              <p className='text-white pb-3 text-sm items-center flex gap-1'><BsTicketPerforated/>Only {/* even.tickets */} tickets left</p>
+              <p className='text-white text-sm items-center flex gap-1'><FaRegClock/>9:00 PM - 10:00 PM{/* even.time */}</p>
             </div>
             <section className='flex flex-col items-end opacity-90'>
+              {/* Aqui van los precios */}
               <h3 className='font-semibold text-white'>Tickets from</h3>
               <p className='text-sm text-white'>140.000 COP</p>
               <p className='text-sm text-white'>30.72 USDT</p>
@@ -83,15 +80,17 @@ function DetailsEvent() {
         </div>
         <form onSubmit={handleBuy} className='flex flex-wrap justify-between '>
           <div className='flex flex-col items-start opacity-90'>
+            {/* Moneda de pago */}
             <p className='text-sm text-white py-3'>Your payment will be made in</p>
             <select className='text-white text-sm mb-2 bg-[#55347b] border py-1 px-3'>
               <option value="COP" >COP</option>
             </select>
           </div>
-
-          <p className='text-sm text-white p-3'>Only 10 tickets left </p>
+            {/* Stocks */}
+          <p className='text-sm text-white p-3'>Only {/* even.tickets */}tickets left </p>
           <div className='w-screen flex justify-between items-center border-b-2 border-[#8468fb] pb-3'>
             <div>
+              {/* Cantidades a comprar */}
               <p className='text-sm text-white text-center pb-1'>Quantity</p>
               <div className='flex'>
               <IoMdRemoveCircleOutline onClick={handleSubtract} className='text-white text-2xl' />
@@ -99,9 +98,13 @@ function DetailsEvent() {
               <CgAdd onClick={handleAdd} className='text-white text-2xl'/>
               </div>
             </div>
-            
+            {/* Tipo de ticket */}
             <select className='text-white bg-[#55347b] text-sm border py-1 px-1'>
               <option value="">General</option>
+              {
+                /* even.ticketTypes.map((type, index) => (
+                  <option value={type} key={index}>{type}</option>)) */
+              }
             </select>
             <div>
               <p className='text-sm text-white'>Total with taxes</p>
